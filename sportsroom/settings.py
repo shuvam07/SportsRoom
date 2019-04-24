a981b324dc653560b899a491e286434307c0c29d
raul1rnjn535_3 <ranjanraul10@gmail.com>
2019-04-25 03:31:55 +0530

    logfile added

--- sportsroom/settings.py
+++ sportsroom/settings.py
@@ -139,3 +139,64 @@ EMAIL_HOST_PASSWORD = 'abc'
 DAILY_PENALTY = 5
 RETURN_PERIOD = 1
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+LOGGING = {
+  'version': 1,
+  'disable_existing_loggers': False,
+  'formatters': {
+      'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+        'standard': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s %(filename)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+  },
+  'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'file': {
+            'level':'INFO',
+            'class':'logging.FileHandler',
+            'filename': BASE_DIR + "/logfile",
+            #'maxBytes': 50000,
+            #'backupCount': 2,
+            'formatter': 'standard',
+        },
+        #'logstash': {
+        #    'level': 'INFO',
+        #    'class': 'logstash.TCPLogstashHandler',
+        #    'host': '127.0.0.1',
+        #    'port': 5959, #Default value: 5959
+        #    'version': 1, # Version of logstash event schema. Default value: 0 (for backward compatibility of the library)
+        #    'message_type': 'django_logstash',  # 'type' field in logstash message. Default value: 'logstash'.
+        #    'fqdn': False, # Fully qualified domain name. Default value: false.
+        #    'tags': ['django.request'], # list of tags. Default: None.
+        #},
+  },
+  'loggers': {
+        #'django.request': {
+        #    'handlers': ['logstash'],
+        #    'level': 'INFO',
+        #    'propagate': True,
+        #},
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    }
+}
